de8a33c16f4989bcc2ad7b12f9cc990dd4429ff9
RubenVives <belialh666@gmail.com>
2017-05-28 16:49:35 +0200

    Añadido: hola soy Jhon otra vez

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -17,6 +17,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola soy jhon");
+        System.out.println("hola soy jhon otra vez");
         
     }
     
